665288e6c913d312c4134f7de651087fa91a282f
sasa73d <sasa.ilic73d@gmail.com>
2017-12-25 12:42:30 +0100

    rest api
    rest api project

--- izba1/js/controllers/user-controller.js
+++ izba1/js/controllers/user-controller.js
@@ -1,9 +1,10 @@
 angular.module('HeadModule')
 .controller('UserCtrl', ['$scope','$http', 'passData', function($scope, $http, passData) {
-    var urlConection = 'https://jsonplaceholder.typicode.com/users';
+    var urlConection = 'http://localhost:8080/first-rest/webapi/users';
     $http.get(urlConection).then(function(response) {
         $scope.users = response.data;
         passData.setUsers($scope.users);
+        log(users);
     });
     $scope.passUser = function() {
         if ($scope.selected == null) {
